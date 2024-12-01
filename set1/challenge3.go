61dfb9364fcd5de0eb280cb3f069dc02e5b07963
Florian <florian@batch.com>
2024-12-01 14:09:33 +0100

    set1 chal3 calculate score from regexp for each message

--- set1/challenge3.go
+++ set1/challenge3.go
@@ -2,20 +2,29 @@ package set1
 
 import (
 	"ffrancon/cryptopals-go/utils"
-	"fmt"
+	"regexp"
 )
+
+type message struct {
+	key       byte
+	decrypted []byte
+	score     int
+}
 
 func Challenge3(str string) {
 	bytes := utils.HexStrToBytes(str)
 	if bytes == nil {
 		return
 	}
+	messages := make([]message, 256)
+	reg := regexp.MustCompile(`(?i)[ETAOIN SHRDLU]`)
 	for i := 0; i < 256; i++ {
 		copy := make([]byte, len(bytes))
 		byte := byte(i)
 		for j := range bytes {
 			copy[j] = bytes[j] ^ byte
 		}
-		fmt.Printf("%d => %s\n", i, string(copy[:]))
+		score := len(reg.FindAll(copy, -1))
+		messages[byte] = message{byte, copy, score}
 	}
 }
